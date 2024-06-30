e1bddd60eea8cd49f8dc2019385ef5b666a0289d
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-30 09:59:55 -0300

    feat: :sparkles: Add delete, inactive, get and getAll methods to doctor crud

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -1,8 +1,11 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import med.voll.api.domain.doctor.Doctor;
+import med.voll.api.domain.doctor.DoctorEntity;
 import med.voll.api.domain.doctor.DoctorResponse;
+import med.voll.api.domain.doctor.UpdateDoctor;
 import med.voll.api.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,7 +13,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.Doc;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/doctors")
@@ -26,5 +31,25 @@ public class DoctorController {
     @GetMapping
     public Page<DoctorResponse> getAll(@PageableDefault(size = 10, page = 0, sort = {"name"}) Pageable pagination) {
         return doctorService.getAllDoctors(pagination);
+    }
+
+    @GetMapping("/{id}")
+    public DoctorResponse get(@PathVariable("id") UUID id) {
+        return doctorService.get(id);
+    }
+
+    @PutMapping("/{id}")
+    public DoctorEntity update(@PathVariable("id") UUID id, @RequestBody UpdateDoctor request) {
+        return doctorService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") UUID id) {
+        doctorService.delete(id);
+    }
+
+    @DeleteMapping("/{id}/inactive")
+    public void inactive(@PathVariable("id") UUID id) {
+        doctorService.inactivate(id);
     }
 }
--- src/main/java/med/voll/api/service/DoctorService.java
+++ src/main/java/med/voll/api/service/DoctorService.java
@@ -1,9 +1,9 @@
 package med.voll.api.service;
 
-import med.voll.api.domain.address.AddressEntity;
 import med.voll.api.domain.doctor.Doctor;
 import med.voll.api.domain.doctor.DoctorEntity;
 import med.voll.api.domain.doctor.DoctorResponse;
+import med.voll.api.domain.doctor.UpdateDoctor;
 import med.voll.api.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,9 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
 @Service
 public class DoctorService {
@@ -28,6 +26,36 @@ public class DoctorService {
     }
 
     public Page<DoctorResponse> getAllDoctors(Pageable pagination) {
-        return doctorRepository.findAll(pagination).map(DoctorResponse::new);
+        return doctorRepository.findAllByActiveTrue(pagination).map(DoctorResponse::new);
+    }
+
+    public DoctorResponse get(UUID id) {
+        var doctor = doctorRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Doctor not found"));
+        return new DoctorResponse(
+            doctor
+        );
+    }
+
+    @Transactional
+    public DoctorEntity update(UUID id, UpdateDoctor request) {
+        var doctor = doctorRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Doctor not found"));
+        doctor.update(request);
+
+        return doctor;
+    }
+
+    @Transactional
+    public void delete(UUID id) {
+        doctorRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void inactivate(UUID id) {
+        var doctor = doctorRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Doctor not found"));
+
+        doctor.inactivate();
     }
 }
