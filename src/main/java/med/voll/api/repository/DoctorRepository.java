a93b16d9ecb64f89c72adb9507daf721f67b364c
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-30 09:59:21 -0300

    feat: :sparkles: Add filter by active true in get all doctors

--- src/main/java/med/voll/api/repository/DoctorRepository.java
+++ src/main/java/med/voll/api/repository/DoctorRepository.java
@@ -1,9 +1,13 @@
 package med.voll.api.repository;
 
 import med.voll.api.domain.doctor.DoctorEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface DoctorRepository extends JpaRepository<DoctorEntity, UUID> {
+
+    Page<DoctorEntity> findAllByActiveTrue(Pageable pageable);
 }
