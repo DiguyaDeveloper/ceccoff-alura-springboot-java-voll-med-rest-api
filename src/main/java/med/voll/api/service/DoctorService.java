a354166b3f0b62032ac06b825d2dd1cd2f648210
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-29 22:07:12 -0300

    feat: :sparkles: Add spring transactional

--- src/main/java/med/voll/api/service/DoctorService.java
+++ src/main/java/med/voll/api/service/DoctorService.java
@@ -6,6 +6,7 @@ import med.voll.api.domain.doctor.DoctorEntity;
 import med.voll.api.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DoctorService {
@@ -13,6 +14,7 @@ public class DoctorService {
     @Autowired
     private DoctorRepository doctorRepository;
 
+    @Transactional
     public void create(Doctor doctor) {
         DoctorEntity newDoctor = new DoctorEntity(doctor);
         doctorRepository.save(newDoctor);
