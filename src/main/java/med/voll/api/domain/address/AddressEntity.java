ea89e0610752272f56d56c4bd477f28b08378356
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-30 09:58:12 -0300

    feat: :sparkles: Add update address

--- src/main/java/med/voll/api/domain/address/AddressEntity.java
+++ src/main/java/med/voll/api/domain/address/AddressEntity.java
@@ -27,4 +27,28 @@ public class AddressEntity {
         this.number = address.number();
         this.additionalInfo = address.additionalInfo();
     }
+
+    public void update(Address address) {
+        if (address.street() != null) {
+            this.street = address.street();
+        }
+        if (address.neighborhood() != null) {
+            this.neighborhood = address.neighborhood();
+        }
+        if (address.zipCode() != null) {
+            this.zipCode = address.zipCode();
+        }
+        if (address.city() != null) {
+            this.city = address.city();
+        }
+        if (address.state() != null) {
+            this.state = address.state();
+        }
+        if (address.number() != null) {
+            this.number = address.number();
+        }
+        if (address.additionalInfo() != null) {
+            this.additionalInfo = address.additionalInfo();
+        }
+    }
 }
