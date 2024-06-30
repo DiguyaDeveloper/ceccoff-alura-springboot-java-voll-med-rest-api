ab22485be05873a7957db8535613ce8454bbb87d
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-06-30 09:58:27 -0300

    feat: :sparkles: Add id in response of list all doctors

--- src/main/java/med/voll/api/domain/doctor/DoctorResponse.java
+++ src/main/java/med/voll/api/domain/doctor/DoctorResponse.java
@@ -2,7 +2,10 @@ package med.voll.api.domain.doctor;
 
 import med.voll.api.domain.doctor.enums.Speciality;
 
+import java.util.UUID;
+
 public record DoctorResponse(
+        UUID id,
         String name,
         String email,
         String crm,
@@ -10,6 +13,7 @@ public record DoctorResponse(
 ) {
         public DoctorResponse(DoctorEntity doctor) {
                 this(
+                        doctor.getId(),
                         doctor.getName(),
                         doctor.getEmail(),
                         doctor.getCrm(),
