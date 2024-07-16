006630fc8e458266e25734a277fbfd766bd96e51
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-07-15 22:29:31 -0300

    feat: :sparkles: Update response to use ResponseEntity

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -2,16 +2,15 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import jakarta.websocket.server.PathParam;
-import med.voll.api.domain.doctor.Doctor;
-import med.voll.api.domain.doctor.DoctorEntity;
-import med.voll.api.domain.doctor.DoctorResponse;
-import med.voll.api.domain.doctor.UpdateDoctor;
+import med.voll.api.domain.doctor.*;
 import med.voll.api.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.print.Doc;
 import java.util.List;
@@ -24,13 +23,19 @@ public class DoctorController {
     @Autowired
     private DoctorService doctorService;
     @PostMapping
-    public void create(@RequestBody @Valid Doctor request) {
-        doctorService.create(request);
+    public ResponseEntity<DoctorResponse> create(@RequestBody @Valid Doctor request, UriComponentsBuilder uriBuilder) {
+        DoctorEntity doctor = doctorService.create(request);
+
+        var uri = uriBuilder.path("/doctors/{id}").buildAndExpand(doctor.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DoctorResponse(doctor));
     }
 
     @GetMapping
-    public Page<DoctorResponse> getAll(@PageableDefault(size = 10, page = 0, sort = {"name"}) Pageable pagination) {
-        return doctorService.getAllDoctors(pagination);
+    public ResponseEntity<Page<DoctorResponse>> getAll(@PageableDefault(size = 10, page = 0, sort = {"name"}) Pageable pagination) {
+        Page<DoctorResponse> doctors =  doctorService.getAllDoctors(pagination);
+
+        return ResponseEntity.ok(doctors);
     }
 
     @GetMapping("/{id}")
@@ -39,17 +44,23 @@ public class DoctorController {
     }
 
     @PutMapping("/{id}")
-    public DoctorEntity update(@PathVariable("id") UUID id, @RequestBody UpdateDoctor request) {
-        return doctorService.update(id, request);
+    public ResponseEntity<DoctorResponse> update(@PathVariable("id") UUID id, @RequestBody UpdateDoctor request) {
+        DoctorEntity doctor = doctorService.update(id, request);
+
+        return ResponseEntity.ok(new DoctorResponse(doctor));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") UUID id) {
+    public ResponseEntity delete(@PathVariable("id") UUID id) {
         doctorService.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}/inactive")
-    public void inactive(@PathVariable("id") UUID id) {
+    public ResponseEntity inactive(@PathVariable("id") UUID id) {
         doctorService.inactivate(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
